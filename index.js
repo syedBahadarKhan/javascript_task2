8ee96206d0c7ce17c4e18ff4bfa46db18e9ebfdc
syedBahadarKhan <ba4646465@gmail.com>
2024-09-19 15:31:21 +0500

    add this

---
+++ index.js
@@ -0,0 +1,9 @@
+function firstUse(){
+    var store = document.getElementById("first");
+    store.classList.add("dark");
+};
+
+function secondUse(){
+    var light= document.getElementById("first");
+    light.classList.add("white");
+}
